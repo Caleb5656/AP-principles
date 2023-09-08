6d300ab85625d2b22ad47b2b61f22a22c74a4ab2
raddatz.c <calebraddatz06@gmail.com>
2023-09-08 09:33:34 -0500

    update

---
+++ Prog54b.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter a three digit number: "))
+num2 = int(input("Enter a three digit number: "))
+num3 = int(input("Enter a three digit number: "))
+num4 = int(input("Enter a three digit number: "))
+
+sum = num1 + num2 + num3 + num4
+avg = sum/4
+
+print("The sum of the four numbers is: ", sum, "\n The avg is: ", avg)
+
+# Enter a three digit number: 475
+# Enter a three digit number: 821
+# Enter a three digit number: 369
+# Enter a three digit number: 562
+# The sum of the four numbers is:  2227
+#  The avg is:  556.75
