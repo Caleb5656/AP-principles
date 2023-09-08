a1dcda3a9f4634451eea3d6f55fa39bc8ef0941a
raddatz.c <calebraddatz06@gmail.com>
2023-09-08 09:13:10 -0500

    update

---
+++ Prog52a.py
@@ -0,0 +1,16 @@
+print("Enter a length: ")
+len = int(input())
+print("Enter a width: ")
+wid = int(input())
+
+area = len * wid
+perim = len + len + wid + wid
+
+print("The area is: ", area)
+print("The perimeter is: ", perim)
+#Enter a length:
+#7
+#Enter a width:
+#4
+#The area is:  28
+#The perimeter is:  22
