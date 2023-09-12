0d82a75e81c75b64516243a24670cf52ef6d529f
raddatz.c <calebraddatz06@gmail.com>
2023-09-12 09:37:29 -0500

    update

--- 58h.py
+++ 58h.py
@@ -2,7 +2,7 @@ p = float(input("Amount saved: "))
 r = float(input("Interest rate: "))/100
 n = float(input("Number of days compounded per year: "))
 t = float(input("The number of days earning interest: "))
-exp = ((n*t)/365)
-A = p * ((1 +(pow((.01* r)/n, exp))))
+
+A = p * (1 + (r / n)) ** (n * (t / 365))
 print("The amount of interest earned is: ", A)
 # ::-1 for inverting string
