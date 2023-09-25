28b5506c0143d730e024d81a5e035577b9deb728
raddatz.c <raddatz.c@stu.janesville.k12.wi.us>
2023-09-25 09:38:14 -0500

    update

--- 122d.py
+++ 122d.py
@@ -1,2 +1,4 @@
-x = -12
-y = pow(x, 6) - (3 * pow(x,5))
+
+for x in range(-12, 17):
+    y = pow(x, 6) - (3 * pow(x,5)) - (93 * pow(x,3)) + (87 * pow(x, 3)) + (1596 * pow(x, 2)) - (1380 * x) - 2800
+    print(y)
