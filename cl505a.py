1786ad31ebfe72949e11ce12d4906eab6b5f797b
raddatz.c <raddatz.c@stu.janesville.k12.wi.us>
2023-10-18 09:32:57 -0500

    update

--- cl505a.py
+++ cl505a.py
@@ -4,4 +4,11 @@ class books:
         self.b = b
         self.points = 0
     def point(self):
-        if self.b <= 3:
+        if int(self.b) <= 3:
+            self.points = self.b * 10
+        elif int(self.b) <= 6:
+            self.points = (self.b - 3)* 15 + 30
+        elif int(self.b) > 6:
+            self.points = (self.b-6)*20 + 75
+            pass
+        pass
--- prog505a.py
+++ prog505a.py
@@ -1,22 +1,37 @@
+import cl505a
+from cl505a import books
 
-
-
-def calcp(book):
+def win(arr):
+    arr = []
+    win = ""
+    w = 0
+    for b in arr:
+        if b.points > w:
+            win = b.name
+            w = b.point
+            pass
+        pass
+    return win
 
 
 def main():
     try:
-
+        book = []
         with open("data/prog505a", 'r') as f:
             for line in f:
                 ldata = line.split(" ")
                 name = ldata[0] + " " + ldata[1]
-                books = 
+                books = int(ldata[2])
+                hi = cl505a.books(name, books)
+                book.append(hi)
+            avg = 0
+            for b in book:
+                b.point()
+                avg += b.points
 
-
-
-
-
+            avg /= len(book)
+            winner = win(book)
+            print()
     except Exception as e:
         print("Error:", e)
 
