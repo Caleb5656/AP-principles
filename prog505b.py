160d8f54f76d7cec7641f1bcccbbc4a792b88f9d
raddatz.c <raddatz.c@stu.janesville.k12.wi.us>
2023-10-20 09:25:22 -0500

    update

--- prog505b.py
+++ prog505b.py
@@ -1,6 +1,23 @@
+#import pandas as pd
+#df = pd.read_csv("data/prog505b", delim_whitespace=True, names=[] )
 try:
     with open("data/prog505b", 'r') as f:
+        grade = []
+        person = []
         for line in f:
-            print()
+            name = line + " " + (line + 1)
+            line += 2
+            g1 = int(line)
+            line += 1
+            g2 = int(line)
+            line += 1
+            g3 = int(line)
+            line +=1
+            g4 = int(line)
+            line +=1
+            g5 = int(line)
+            person.append(name)
+            grade.append(g1, g2, g3, g4, g5)
+
 except Exception as e:
     print("Error:", e)
