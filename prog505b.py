f991f2a4751b2e6c1bb9d25788e7bd5e5bcc9205
Caleb5656 <112715590+Caleb5656@users.noreply.github.com>
2023-10-24 14:02:22 +0000

    update

--- prog505b.py
+++ prog505b.py
@@ -1,10 +1,11 @@
-#import pandas as pd
+#mport pandas as pd
 #df = pd.read_csv("data/prog505b", delim_whitespace=True, names=[] )
 try:
     with open("data/prog505b", 'r') as f:
         grade = []
-        person = []
+        
         for line in f:
-
+            hi = line
+            grade.append(hi)
 except Exception as e:
     print("Error:", e)
