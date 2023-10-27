16cc94a1bc264dd9353fd7c019318bf3342b12df
Caleb <calebraddatz06@gmail.com>
2023-10-27 01:05:36 -0500

    update

--- strint16.py
+++ strint16.py
@@ -1,2 +1,10 @@
 str1 = input("Enter a string: ")
 str2 = input("Enter a second string: ")
+doesContain = False
+
+contains = str1.find(str2)
+if contains != -1: doesContain = True
+print("The first string is contained in the second string: ", doesContain)
+# Enter a string: hello
+# Enter a second string: lo
+# The first string is contained in the second string:  True
