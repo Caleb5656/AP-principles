8cc177a837037235dd229c136d828b3ec3fe3bb6
Caleb <calebraddatz06@gmail.com>
2023-12-12 23:48:27 -0600

    update

---
+++ Project 2/Gradebook.py
@@ -0,0 +1,8 @@
+#Gradebook: Store, modify, and calculate student grades.
+# calc GPA
+# calc Grade in class
+def main():
+    name =
+
+if __name__ == 'main':
+    main()
---
+++ Project 2/Student.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, stu_name,schedule,grades):
+        self.name = stu_name
+        self.sched = schedule
+        self.grade = grades
+
+    #def calc_gpa(self, grades):
