068df24f57aa50757bf3d3291e6a61e6bc8ca239
Caleb5656 <112715590+Caleb5656@users.noreply.github.com>
2023-12-14 14:57:38 +0000

    update

--- Project 2/Gradebook.py
+++ Project 2/Gradebook.py
@@ -1,6 +1,12 @@
+import tkinter as tk
+import tkinter.messagebox
+from tkinter import ttk
 #Gradebook: Store, modify, and calculate student grades.
 # calc GPA
 # calc Grade in class
+root = tk.Tk()
+root.title("Gradebook")
+
 def main():
     name = input("Enter the name of the student: ")
     Schedule = []
