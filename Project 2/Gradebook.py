b8e462856b53d0fa5c2e106b78b6edd00f372382
raddatz.c <raddatz.c@stu.janesville.k12.wi.us>
2023-12-19 09:38:17 -0600

    update

--- Project 2/Gradebook.py
+++ Project 2/Gradebook.py
@@ -6,12 +6,15 @@ from tkinter import END, ttk
 # calc Grade in class
 def get_name():
     stu_name = stu_add_txt.get()
-    
-    
-    sched_tab = ttk.Frame(tab_control)
-    tab_control.add(sched_tab, text=stu_name)
-    tab_control.pack(expand=1, fill="both")
-    stu_add_txt.delete(0, END)
+    if stu_name == "":
+        msg="Enter a students name first!"
+        msg_box = tk.(text=msg)
+        msg_box.pack()
+    else:
+        sched_tab = ttk.Frame(tab_control)
+        tab_control.add(sched_tab, text=stu_name)
+        tab_control.pack(expand=1, fill="both")
+        stu_add_txt.delete(0, END)
 root = tk.Tk()
 root.geometry("300x300")
 root.title("Gradebook")
@@ -29,10 +32,11 @@ tab_control.pack(expand=1, fill="both")
 
 stu_add_lbl = tk.Label(reg_tab,text="Enter the students name into the box", bg='blue')
 stu_add_lbl.pack()
-stu_add_txt = tk.Entry(reg_tab)
+stu_add_txt = tk.Entry(reg_tab,borderwidth=5)
 stu_add_txt.pack()
 
-stu_add_btn = tk.Button(reg_tab, text="Add Student", command=get_name, bg='grey')
+stu_add_btn = tk.Button(reg_tab, text="Add Student", command=get_name, bg='grey', width=10, height=4)
+stu_add_btn.place(x=150,y=-32)
 stu_add_btn.pack()
 
 
