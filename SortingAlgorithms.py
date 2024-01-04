d9f223f5e4a528f001169d318984f5e994c39b0d
raddatz.c <raddatz.c@stu.janesville.k12.wi.us>
2024-01-04 09:38:29 -0600

    update

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -1,7 +1,8 @@
-class sort:
+class Sort:
     def __init__(self, other):
         self.other = other
-    def bubble(self):
+
+    def bubble(self) -> object:
         for i in range(0, len(self.other) - 1):
             for j in range(0, len(self.other) - i - 1):
                 if self.other[j] > self.other[j + 1]:
@@ -9,3 +10,17 @@ class sort:
                     self.other[j] = self.other[j + 1]
                     self.other[j + 1] = temp
         return self.other
+
+    def SelectionSort(A):
+        for i in range (0,len(A) - 1):
+            min_index = i
+        for j in range(i,len(A)):
+            if A[j] < A[min_index]:
+                min_index = j
+            Swap
+            A[i] and A[min
+            index]
+            end
+            for
+                end
+                procedure
--- cl408a.py
+++ cl408a.py
@@ -1,6 +1,9 @@
-class cl408a:
+class allsort:
     def __init__(self, id, score):
         self.id = id
         self.score = score
+
     def __lt__(self, other):
         return self.score < other.score
+
+
