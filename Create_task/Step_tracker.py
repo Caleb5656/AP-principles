7b33c42076777edcab272dd4c8d38f02e465755a
raddatz.c <raddatz.c@stu.janesville.k12.wi.us>
2024-03-25 09:34:41 -0500

    update

--- Create_task/Step_tracker.py
+++ Create_task/Step_tracker.py
@@ -14,24 +14,29 @@ def main():
     print("-1 to stop adding or end program")
     print("1. Add to step tracker")
     print("2. get Average steps taken")
-    task = int(input("Select an option(0, 1, or 2): "))
-    num = 0
-    while task != -1:
-        if task == 1:
+    task = int(input("Select an option(-1, 1, 2, or quit): "))
+
+    while input() != "quit":
+        num = 0
+        while task == 1:
             while num != -1:
                 num = int(input("Enter the number of steps you've taken: "))
-                if num != -1:
-                    steps.append(num)
-            task = input("Enter new task number: ")
-        elif task == 2:
+                if num == -1:
+                    task = input("Enter new task number: ")
+                    break
+                steps.append(num)
+            break
+        if task == 2:
             if len(steps) == 0:
                 print("Cannot take the average without any data!")
+                task = input("Enter new task number: ")
+                pass
             elif len(steps) > 0:
                 avg = average(steps)
                 if isEnough(avg) == True:
                     print(f"Congrats you have been taking plenty of steps with your average being {avg}")
                 else:
                     print(f"Try to get those steps up your average is {avg} try to aim for at least 7000 ")
-            print("Enter new task number: ")
+                print("Enter new task number: ")
 if __name__=="__main__":
     main()
