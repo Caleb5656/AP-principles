9192b57f62af0130a49a6c12421da53e0b68b9c6
raddatz.c <raddatz.c@stu.janesville.k12.wi.us>
2024-03-26 09:37:41 -0500

    update

--- Create_task/Step_tracker.py
+++ Create_task/Step_tracker.py
@@ -9,34 +9,35 @@ def average(step):
     for i in step:
         avg += i
     avg = int(avg/len(step))
+    return avg
 def main():
     steps = []
     print("-1 to stop adding or end program")
     print("1. Add to step tracker")
     print("2. get Average steps taken")
-    task = int(input("Select an option(-1, 1, 2, or quit): "))
+    task = int(input("Select an option(-1, 1, 2): "))
 
-    while input() != "quit":
+    while task != -1:
         num = 0
-        while task == 1:
+        if task == 1:
             while num != -1:
                 num = int(input("Enter the number of steps you've taken: "))
                 if num == -1:
-                    task = input("Enter new task number: ")
-                    break
+                    task = int(input("Enter new task number: "))
+                    num = -1
                 steps.append(num)
-            break
         if task == 2:
             if len(steps) == 0:
                 print("Cannot take the average without any data!")
-                task = input("Enter new task number: ")
-                pass
+                task = int(input("Enter new task number: "))
             elif len(steps) > 0:
                 avg = average(steps)
-                if isEnough(avg) == True:
-                    print(f"Congrats you have been taking plenty of steps with your average being {avg}")
+                if isEnough(avg):
+                    print(f"Congrats you have been taking plenty of steps "
+                          f"with your average being {avg}")
                 else:
-                    print(f"Try to get those steps up your average is {avg} try to aim for at least 7000 ")
-                print("Enter new task number: ")
+                    print(f"Try to get those steps up your average is "
+                          f"{avg} try to aim for at least 7000 ")
+                task = int(input("Enter new task number: "))
 if __name__=="__main__":
     main()
