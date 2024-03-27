aeaf1bfff2cc585e0a9706e9b5d301abccd71258
Caleb5656 <112715590+Caleb5656@users.noreply.github.com>
2024-03-27 19:30:31 +0000

    UPDATE

--- MSOE2010_6.py
+++ MSOE2010_6.py
@@ -7,4 +7,11 @@ for i in range(0,n-1):
     x_coord.append(x)
     y_coord.append(y)
 
+x_coord.append(x_coord[0])
+y_coord.append(y_coord[0])
+
 A=0.0
+for i in range (0,n-1):
+    A +=((x_coord[i]*y_coord[i+1])-(x_coord[i+1]*y_coord[i]))
+A = .5*A
+print("The area of the polygon is: ", A)
